ccd2a65dff76c023a7f41a7ff7101c886b6ab06a
Saul Gustavo <saulgustavo237@gmail.com>
2025-03-06 18:57:30 -0600

    se modifico el vite

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/insure-landing-page/",
+  base: './',  // Usa "./" en lugar de "/insure-landing-page/" si subes todo a GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
+  },
+  build: {
+    outDir: 'dist',
   },
 });
