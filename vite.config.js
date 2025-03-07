ef475f9d7878d2003df230ea9c63af9a94da49e2
Saul Gustavo <saulgustavo237@gmail.com>
2025-03-06 20:28:28 -0600

    Se agregaron nuevas funciones

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 export default defineConfig({
-  base: './',  // Usa "./" en lugar de "/insure-landing-page/" si subes todo a GitHub Pages
+  base: "https://saul-gustavo.github.io/insure-landing-page",  // Usa "./" en lugar de "/insure-landing-page/" si subes todo a GitHub Pages
   plugins: [react()],
   resolve: {
     alias: {
